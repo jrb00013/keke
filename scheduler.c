8e780e94161a83bd5bb48f73510d6910a737b7b7
Joe Black <156119500+jrb00013@users.noreply.github.com>
2025-03-19 11:37:55 -0400

    Delete scheduler.c

--- scheduler.c
+++
@@ -1,8 +0,0 @@
-#include "rtos_kernel.h"
-
-void schedule_tasks() {
-    // Basic scheduling logic, for instance, round-robin
-    for (int i = 0; i < task_count; i++) {
-        task_list[i].task_function();
-    }
-}
